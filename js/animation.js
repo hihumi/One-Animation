9e7d1edec17c1573373481eb6c3a6b362d1c9ab3
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-21 08:58:09 +0900

    first commit

---
+++ js/animation.js
@@ -0,0 +1,23 @@
+jQuery.noConflict();
+(function($) {
+  $(function() {
+      $("#startAnimation").click(function() {
+        $("#footer").text(":D");
+        for (var i = 0; i < 10; i++) {
+          $("#bluePng").hide(1500);
+          $("#bluePng").show(2000);
+        
+             $("#stopAnimation").click(function() {
+               $("#bluePng").stop();
+             });
+        }
+  });
+
+  $(function() {
+    $("#reloadBtn").click(function() {
+      location.reload();
+    });
+  });
+
+});
+})(jQuery);
